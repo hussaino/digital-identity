2c032a085b51faaf4ad74dff940ce6b183f310fe
Yasir Ghunaim <yasirghunaim@gmail.com>
2020-04-20 19:25:27 +0300

    absher_business_app: Added loader spinner

--- absher_business_app/src/components/HomeComponent.tsx
+++ absher_business_app/src/components/HomeComponent.tsx
@@ -1,20 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom'
+import { IonLoading, IonContent } from '@ionic/react';
 import './HomeComponent.css';
 
 const Component: React.FC = () => {
 
   const history = useHistory();
+  const [showLoading, setShowLoading] = useState(false);
 
-  const openCustomerDetails = (e: any) => {
-    e.preventDefault();
+  const openCustomerDetails = () => {
+    //e.preventDefault();
+    setShowLoading(false);
     history.push('/home/customer');
   }
 
+
   return (
-    <div className="home-container">
-      <img src="assets/image/absher-home.jpeg" height="800" onClick={e => openCustomerDetails(e)}></img>
-    </div>
+    <IonContent>
+      <IonLoading
+        isOpen={showLoading}
+        onDidDismiss={() => openCustomerDetails()}
+        message={'...انتظار موافقة العميل'}
+        duration={3000}
+      />
+      <div className="home-container">
+        <img src="assets/image/absher-home.jpeg" height="800" onClick={e => setShowLoading(true)}></img>
+      </div>
+    </IonContent>
   );
 };
 
