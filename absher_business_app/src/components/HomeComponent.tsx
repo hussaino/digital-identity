7c4f81a143ecc7a439d6fbd918fdb03e436ac0d0
Yasir Ghunaim <yasirghunaim@gmail.com>
2020-04-24 13:55:53 +0300

    absher_business_app: Implemented requestCustomerInformation API call

--- absher_business_app/src/components/HomeComponent.tsx
+++ absher_business_app/src/components/HomeComponent.tsx
@@ -2,23 +2,40 @@ import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom'
 import { IonLoading, IonContent } from '@ionic/react';
 import { BarcodeScanner } from '@ionic-native/barcode-scanner';
+import { establishWebSocket, requestCustomerInformation } from '../serverside'
 import './HomeComponent.css';
-
 
 const Component: React.FC = () => {
 
   const history = useHistory();
   const [showLoading, setShowLoading] = useState(false);
-
-  const openScanner = async () => {
-    const data = await BarcodeScanner.scan();
-    console.log(`Barcode data: ${data.text}`);
-  };
+  const [qrContent, setQrContent] = useState("");
 
   // Run once on component start
   useEffect(() => {
-    openScanner();
+    openQRScanner();
   }, []);
+
+
+  const openQRScanner = async () => {
+    const data = await BarcodeScanner.scan();
+    console.log(`Barcode data: ${data.text}`);
+
+    // Establish a websocket connection 
+    if (data.text.length > 0) {
+      setShowLoading(true);
+      setQrContent(data.text);
+
+      establishWebSocket(handleWebSocketResponse); // Open a WebSocket with the server
+    }
+  };
+
+  const handleWebSocketResponse = (response: any) => {
+    console.log(`handleWebSocketResponse - ID: ${response.connectionId}`);
+
+    // Request information from customer
+    requestCustomerInformation(response.connectionId, qrContent);
+  }
 
 
   const openCustomerDetails = () => {
@@ -32,9 +49,7 @@ const Component: React.FC = () => {
     <IonContent>
       <IonLoading
         isOpen={showLoading}
-        onDidDismiss={() => openCustomerDetails()}
         message={'...انتظار موافقة العميل'}
-        duration={3000}
       />
     </IonContent>
   );
---
+++ absher_business_app/src/serverside.ts
@@ -0,0 +1,44 @@
+import Amplify, { API } from 'aws-amplify';
+import awsmobile from './aws-exports';
+Amplify.configure(awsmobile);
+
+
+// ------------------- WebSocket -------------------
+export const establishWebSocket = (callback: any) => {
+    var wss = new WebSocket('wss://sgyb5djk3h.execute-api.eu-central-1.amazonaws.com/dev');
+    wss.onmessage = (event) => {
+        console.log(JSON.parse(event.data));
+        callback(JSON.parse(event.data))
+    }
+    wss.onerror = (error) => console.log({ error });
+    wss.onopen = () => wss.send(JSON.stringify({ action: 'connectionId' }));
+}
+
+
+// ------------------- API calls -------------------
+export async function requestCustomerInformation(websocketId: String, qrContent: String) {
+    console.log("requestCustomerInformation():");
+    console.log(websocketId);
+    console.log(qrContent);
+    const apiName = 'ApiGatewayRestApi';
+    const path = '/requests';
+    const myInit = {
+        body: {
+            "businessId": "456",
+            "businessWS": websocketId,
+            "customerQR": qrContent
+        },
+        headers: {},
+    };
+    console.log("myInit");
+    console.log(myInit);
+
+
+    return await API.post(apiName, path, myInit).then((response: any) => {
+        console.log("requestCustomerInformation - response:");
+        console.log(response);
+    }).catch((error: any) => {
+        console.log("requestCustomerInformation - error:");
+        console.log(error);
+    });
+}
